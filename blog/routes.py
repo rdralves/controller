2b1deea0e350d1a4c9835e62971d9cb7a587a57f
Rodrigo Alves dos Reis <52412539+rdralves@users.noreply.github.com>
2024-12-27 11:31:50 +0000

    criado o frontend

--- blog/routes.py
+++ blog/routes.py
@@ -1,8 +1,8 @@
-from flask import Blueprint
+from flask import Blueprint, jsonify
 
 bp_crud = Blueprint('crud', __name__)
 
 
-@bp_crud.route('/')
-def index():
-    return 'Hello, World'
+@bp_crud.route('/api/hello', methods=['GET'])
+def hello_world():
+    return jsonify({"message": "Hello, World"})
